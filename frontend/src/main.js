b3bf7a73cd3e0fb82a0d7fa048a4de1eebda76df
정재호 <jaho901@naver.com>
2022-04-26 11:54:54 +0900

    feat: update module

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import store from "./common/lib/store";
+import VueAxios from "./common/lib/axios";
+import axios from "./common/lib/axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(VueAxios, axios).use(store).use(router).mount("#app");
