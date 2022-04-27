c3474806f9cd4055be7a2c7d638b13dde1a1f05b
정재호 <jaho901@naver.com>
2022-04-27 18:47:37 +0900

    feat: update MainPage

--- frontend/src/views/Main/Main.vue
+++ frontend/src/views/Main/Main.vue
@@ -1,17 +1,24 @@
 <template>
   <div style="height: 100vh; width: 100vw; margin: 0px;">
+    <Header></Header>
     <div class="main"></div>
   </div>
 </template>
 
 <script>
+import Header from "../Common/Header.vue";
 export default {
   name: "Main",
+  components: {
+    Header,
+  }
 }
 </script>
 
 <style>
-main {
+.main {
+  width: 100%;
+  height: 30%;
   background-image: url('@/assets/images/Main/Main.jpeg');
   background-size: cover;
 }
