cbfdd3abc54baeaecbfaa5e1a3d3b8cd778fa382
정재호 <jaho901@naver.com>
2022-04-29 17:10:16 +0900

    feat: add OAuth2 Google & Kakao

---
+++ frontend/src/views/Accounts/OAuthFailure.vue
@@ -0,0 +1,22 @@
+<template>
+<div></div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+  name: 'OAuthFailure',
+  setup() {
+    const router = useRouter()
+
+    onMounted(async() => {
+      alert('소셜 로그인에 실패했습니다!')
+      await router.push({ name: "Main"} )
+    })
+
+    return { onMounted }
+  }
+}
+</script>
---
+++ frontend/src/views/Accounts/OAuthSuccess.vue
@@ -0,0 +1,33 @@
+<template>
+<div></div>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+// import { useStore } from 'vuex'
+import { useRouter, useRoute } from 'vue-router'
+
+export default {
+  name: 'OAuthSuccess',
+  setup() {
+    // const store = useStore()
+    const route = useRoute()
+    const router = useRouter()
+
+    onMounted(async() => {
+      const token = route.query.accessToken
+      localStorage.setItem("jwt", token)
+      if (localStorage.getItem("jwt")) {
+        alert(localStorage.getItem("jwt"));
+        // store.commit("root/MAIN_IS_SIGNIN", true) // 로그인된 표시
+        // await store.dispatch('root/userGetInfo', token) // "/api/v1/profiles/{memberId}" 요청 보내기
+        await router.push({ name: "Main"} )
+      } else {
+        alert('로그인 실패')
+      }
+    })
+
+    return { onMounted }
+  }
+}
+</script>
